7f2922629a4dc224aa716d031616b274e08b8b50
draculahuy <tranquanghuy011120@gmail.com>
2023-03-30 11:38:24 +0700

    UI details

--- src/views/events/homeItem.js
+++ src/views/events/homeItem.js
@@ -1,38 +1,88 @@
-import React, { Component, useState } from 'react'
+import React from 'react';
 import {
-	View,
-	Text,
-	Image,
-	StyleSheet,
-	Dimensions,
-	TouchableOpacity,
-	ScrollView
-} from 'react-native'
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import { createStackNavigator } from '@react-navigation';
-
+import { useNavigation } from '@react-navigation/native';
 const { width: screenWidth } = Dimensions.get('window');
-const HomeItem =(props,{navigation})=> {
-	let {url, title,time,address} = props.event
-	// const { navigate } = props.navigation;
-	return (
-		<TouchableOpacity onPress={() => {
-			props.navigation.navigate('DetailEvent');
-		}}>
-			<View style={{ height: 300, width: '100%', paddingTop: 20, backgroundColor: 'white', marginTop: 10, borderRadius: 15 }}>
-				<Image style={{ width: screenWidth * 0.95, height: '68%', borderRadius: 10, resizeMode: 'cover', marginLeft: screenWidth * 0.025 }} source={{ uri: url }} />
-				<Text style={{ fontSize: 18, color: 'black', marginLeft: screenWidth * 0.025, fontWeight: 'bold', width: screenWidth * 0.95, height: '16%' }}>{title}</Text>
-				<View style={{ flexDirection: 'row', marginLeft: screenWidth * 0.025, width: screenWidth * 0.95, height: '8%' }}>
-					<Image source={require('../../images/time.png')} style={{ height: 18, width: 18 }} />
-					<Text style={{ fontSize: 15, color: '#444444', marginLeft: 5 }}>{time}</Text>
-				</View>
-				<View style={{ flexDirection: 'row', marginLeft: screenWidth * 0.025, width: screenWidth * 0.95, height: '8%' }}>
-					<Image source={require('../../images/vitri.png')} style={{ height: 18, width: 18 }} />
-					<Text style={{ fontSize: 15, color: '#444444', marginLeft: 5 }}>{address}</Text>
-				</View>
-			</View>
-		</TouchableOpacity>
-	)
-}
+
+const HomeItem = ({ event }) => {
+  let { url, title, time, address } = event;
+  const navigation = useNavigation();
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('DetailEvent');
+      }}>
+      <View
+        style={{
+          height: 300,
+          width: '100%',
+          paddingTop: 20,
+          backgroundColor: 'white',
+          marginTop: 10,
+          borderRadius: 15,
+        }}>
+        <Image
+          style={{
+            width: screenWidth * 0.95,
+            height: '68%',
+            borderRadius: 10,
+            resizeMode: 'cover',
+            marginLeft: screenWidth * 0.025,
+          }}
+          source={{ uri: url }}
+        />
+        <Text
+          style={{
+            fontSize: 18,
+            color: 'black',
+            marginLeft: screenWidth * 0.025,
+            fontWeight: 'bold',
+            width: screenWidth * 0.95,
+            height: '16%',
+          }}>
+          {title}
+        </Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            marginLeft: screenWidth * 0.025,
+            width: screenWidth * 0.95,
+            height: '8%',
+          }}>
+          <Image
+            source={require('../../images/time.png')}
+            style={{ height: 18, width: 18 }}
+          />
+          <Text style={{ fontSize: 15, color: '#444444', marginLeft: 5 }}>
+            {time}
+          </Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            marginLeft: screenWidth * 0.025,
+            width: screenWidth * 0.95,
+            height: '8%',
+          }}>
+          <Image
+            source={require('../../images/vitri.png')}
+            style={{ height: 18, width: 18 }}
+          />
+          <Text style={{ fontSize: 15, color: '#444444', marginLeft: 5 }}>
+            {address}
+          </Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 export default HomeItem;
